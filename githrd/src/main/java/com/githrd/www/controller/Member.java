68883b02faa7f6fdeae7873ec9f7152dd86a0597
LeeHyungJune <99246582+LeeHyungJune@users.noreply.github.com>
2022-06-16 14:46:22 +0900

    commit

--- githrd/src/main/java/com/githrd/www/controller/Member.java
+++ githrd/src/main/java/com/githrd/www/controller/Member.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import javax.servlet.http.*;
 
+import org.slf4j.*;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,9 @@ import com.githrd.www.vo.*;
 @Controller
 @RequestMapping("/member")
 public class Member {
+	
+	private static final Logger memberLog = LoggerFactory.getLogger(Member.class);	//	4.
+	
 	@Autowired
 	MemberDao mDao;
 	@Autowired
@@ -49,7 +54,11 @@ public class Member {
 		
 		int cnt = mDao.getLogin(mVO);
 		if(cnt == 1) {
-			session.setAttribute("SID", mVO.getId());
+			session.setAttribute("SID", mVO.getId());	//	로그인 처리
+			//	로그 처리
+			memberLog.info(mVO.getId() + " 님이 로그인 했습니다.");	//	5.	로그 메세지 출력
+			
+			
 			session.setAttribute("MSG_CHECK", "OK");
 			int count = gDao.getMyCount(mVO.getId());
 			session.setAttribute("CNT", count);
@@ -78,6 +87,7 @@ public class Member {
 		int cnt = mDao.getLogin(mVO);
 		if(cnt == 1) {
 			session.setAttribute("SID", mVO.getId());	//	로그인 처리
+			memberLog.info(mVO.getId() + " 님이 로그인 했습니다.");
 			session.setAttribute("MSG_CHECK", "OK");
 			int count = gDao.getMyCount(mVO.getId());
 			session.setAttribute("CNT", count);
@@ -144,7 +154,11 @@ public class Member {
 	
 	@RequestMapping("/logout.blp")
 	public ModelAndView logout(ModelAndView mv, HttpSession session, String vw, String nowPage) {
+		String sid = (String) session.getAttribute("SID");
+		
 		session.removeAttribute("SID");
+		
+		memberLog.info(sid + " 님이 로그인 했습니다.");
 		
 		if(vw == null) {
 			vw = "/www/";
